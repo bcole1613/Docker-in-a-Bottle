847bd4e5b48780badc0d97abb92a350ca1ae1973
briannacole <bmcole1613@gmail.com>
2023-06-09 20:29:12 -0500

    Rearranged my project structure

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ def hello():
 def get_index():
     return static_file('index.html', root=".")
 
+@route('/css/styles.css')
+def get_styles():
+    return static_file('styles.css', root="./css/")
+
+@route('/css/quartz.css')
+def get_quartz():
+    return static_file('quartz.css', root="./css/")
+
+@route('/assests/img/brianna.jpeg')
+def get_profile_img():
+    return static_file('brianna.jpeg', root="./assets/")
+
+@route('/js/scripts.js')
+def get_js_scripts():
+    return static_file('scripts.js', root="./js/")
+
 # make a function to create a get request route for my resume page
 
 if __name__ == "__main__":
